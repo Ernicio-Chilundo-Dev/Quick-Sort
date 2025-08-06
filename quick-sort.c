db8af96235ca48b3b6bebfedcba457fdf191ef1f
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-08-06 04:00:10 +0200

    Implemented partition function for Quick Sort: - Chooses pivot as last element - Rearranges elements so that smaller elements are left of pivot and larger to the right - Uses swap function to exchange elements - Returns pivot’s final index

--- quick-sort.c
+++ quick-sort.c
@@ -7,3 +7,23 @@ void swap(int *a, int *b)
     *a = *b;
     *b = temp;
 }
+
+// Partition function
+int partition(int arr[], int low, int hight)
+{
+    int pivot = arr[hight];
+    int i = (low - 1);
+
+    for (int j = low; j < hight; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[[j]]);
+        }
+    }
+
+    swap(&arr[i + 1], arr[hight]);
+
+    return (i + 1);
+}
